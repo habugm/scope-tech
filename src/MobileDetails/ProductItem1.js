9a8d79bfac0927ec8a6ea690e541aab0bd68eecd
habugm <haben.gebremedhin@aau.edu.et>
2025-07-16 23:39:24 +0300

    ck

--- src/MobileDetails/ProductItem1.js
+++ src/MobileDetails/ProductItem1.js
@@ -7,26 +7,27 @@ function ProductItem1() {
       <h3 className="display-7 text-uppercase mb-4" style={{textAlign:'center'}}>Iphone 10</h3>
       <div style={{ height: '2px', width: '50%', backgroundColor: '#ADD8E6', margin: '10px auto'}} />
       <div style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-        gap: '1rem'
-      }}>
-        <img
-          src="images/ProductItem1/ProductItem1Detail1.jpg"
-          alt="product-item-1"
-          style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
-        />
-        <img
-          src="images/ProductItem1/ProductItem1Detail2.jpg"
-          alt="product-item-2"
-          style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
-        />
-        <img
-          src="images/ProductItem1/ProductItem1Detail3.jpg"
-          alt="product-item-3"
-          style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
-        />
-      </div>
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+  gap: '1rem'
+}}>
+  <img
+    src="images/ProductItem1/ProductItem1Detail1.jpg"
+    alt="product-item-1"
+    style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+  />
+  <img
+    src="images/ProductItem1/ProductItem1Detail2.jpg"
+    alt="product-item-2"
+    style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+  />
+  <img
+    src="images/ProductItem1/ProductItem1Detail3.jpg"
+    alt="product-item-3"
+    style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+  />
+</div>
+
       
       <h4 style={{textAlign:'center'}}>Specs..</h4>
       <div style={{ height: '2px', width: '100%', backgroundColor: '#ADD8E6', margin: '10px auto'}} />
