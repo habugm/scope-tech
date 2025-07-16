946938a013ebf3c3fd7e526b05f80fa072fc6f10
habugm <haben.gebremedhin@aau.edu.et>
2025-07-16 23:49:20 +0300

    up

--- src/MobileDetails/ProductItem1.js
+++ src/MobileDetails/ProductItem1.js
@@ -5,7 +5,6 @@ function ProductItem1() {
     <div style={{ padding: '6rem' }}>
       <div style={{ height: '2px', width: '50%', backgroundColor: '#ADD8E6', margin: '10px auto'}} />
       <h3 className="display-7 text-uppercase mb-4" style={{textAlign:'center'}}>Iphone 10</h3>
-      <div style={{ height: '2px', width: '50%', backgroundColor: '#ADD8E6', margin: '10px auto'}} />
       <div style={{
   display: 'grid',
   gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
@@ -27,8 +26,6 @@ function ProductItem1() {
     style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
   />
 </div>
-
-      
       <h4 style={{textAlign:'center'}}>Specs..</h4>
       <div style={{ height: '2px', width: '100%', backgroundColor: '#ADD8E6', margin: '10px auto'}} />
        
@@ -36,14 +33,13 @@ function ProductItem1() {
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 1fr)',
         gap: '1rem',
-        textAlign: 'center'
+        textAlign: 'center', paddingBottom:'40px'
       }}>
         <h6>90% Battery</h6>
         <h6>8GB RAM</h6>
         <h6>256 ROM</h6>
       </div>
-      <div style={{ height: '2px', width: '100%', backgroundColor: '#ADD8E6', margin: '10px auto'}} />
-    </div>
+       </div>
   );
 }
 
